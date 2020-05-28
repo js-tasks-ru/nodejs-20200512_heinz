5be936d9746211922f7f38c8e1352139c1f285bb
Heinz <nsamoylenko@jtconsulting.ru>
2020-05-28 17:15:11 +0300

    completed 2-module 2-task

--- 2-module/2-task/LineSplitStream.js
+++ 2-module/2-task/LineSplitStream.js
@@ -4,12 +4,28 @@ const os = require('os');
 class LineSplitStream extends stream.Transform {
   constructor(options) {
     super(options);
+    this.line = '';
   }
 
   _transform(chunk, encoding, callback) {
+    const string = chunk.toString()
+    const lines = string.split(os.EOL)
+
+    if (lines.length > 1) {
+      for (let i = 0; i < lines.length - 1; i++) {
+        this.line += lines[i];
+        this.push(this.line);
+      }
+      this.line = lines[lines.length - 1];
+    } else {
+      this.line += string;
+    }
+
+    callback();
   }
 
   _flush(callback) {
+    callback(null, this.line);
   }
 }
 
